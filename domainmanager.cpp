af8ed5074156ae82698af407e6795fbd6708e3c9
Ólafur Aron Jóhannsson <olafur@johannsson.co>
2016-07-13 23:52:53 +0000

    Update domainmanager.cpp

--- domainmanager.cpp
+++ domainmanager.cpp
@@ -3,6 +3,8 @@
 DomainManager::DomainManager()
 {
     this->domains.insert("johannsson.co", QUrl("http://johannsson.co"));
+    this->domains.insert("johannsson.co", QUrl("http://olafurjohannsson.com"));
+    this->domains.insert("johannsson.co", QUrl("http://faktura.is"));
 
     this->requestManager = new RequestManager();
 }
