974f127a0cbaf5ad35e449574625675174b09c1b
Ólafur Aron Jóhannsson <olafur@johannsson.co>
2016-07-13 23:55:20 +0000

    recv data

--- domainmanager.cpp
+++ domainmanager.cpp
@@ -7,6 +7,15 @@ DomainManager::DomainManager()
     this->domains.insert("johannsson.co", QUrl("http://faktura.is"));
 
     this->requestManager = new RequestManager();
+    connect(this->domains, SIGNAL(RecvData()), this, SLOT(SendData()));
+    for (auto domain, this->domains) {
+        this->requestManager::GET(domain.value());
+    }
+}
+
+DomainManager::RecvData()
+{
+    qDebug() << "recv";
 }
 
 DomainManager::~DomainManager()
